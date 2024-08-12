71263b8d7cfce26d000f51f65cd7e0f61bcbcbd3
sefyan0hack <sefyanhack@gmail.com>
2024-08-12 13:02:52 +0100

    nav bar

--- src/Coms/Title/Title.tsx
+++ src/Coms/Title/Title.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+"use client";
+
+import {useState} from 'react';
 import styles from './Title.module.css'; // Ensure you have this CSS module
+import { assets } from '@/src/assets/assets';
 
 function Title() {
+    const [isBarOpen, setIsBarOpen] = useState(false);
+
     return (
-        <div className={styles.titleContainer}>
-            <p className={styles.Title}> Cosmetic By Imane</p>
-        </div>
+        // <div className={styles.titleContainer}>
+        //     <p className={styles.Title}> Cosmetic By Imane</p>
+        // </div>
+    <div className={styles.titleContainer}>
+      <div className={styles.titleWrapper}>
+        <p className={styles.title}>Cosmetic By Imane</p>
+        <img
+          onClick={(e) => {
+            e.preventDefault(); // Prevent default link behavior
+            setIsBarOpen(prevState => !prevState); // Toggle the state
+          }}
+          className={styles.bar}
+          src={assets.navbar.src}
+          alt="Navigation Icon"
+        />
+      </div>
+      {isBarOpen && <div><p>Bar is pressed</p></div>}
+    </div>
     );
 }
 
--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,7 +1,9 @@
 import Product_img from './a.jpg'
+import navbar from './navbar.png'
 import Whatsappico from './whatsapp.ico'
 
 export const assets = {
     Product_img,
-    Whatsappico
+    Whatsappico,
+    navbar
 }
